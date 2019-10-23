580373b131ff59026945c4e05a94c74ce0152f25
Vigge1320 <56914053+Vigge1320@users.noreply.github.com>
2019-10-23 13:21:38 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Work_ffs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
