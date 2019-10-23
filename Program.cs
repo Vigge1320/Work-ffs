d33784e72eda7f36a9bc06a44c84df3dbf1a1ac4
Stefan Näslund <56830149+StefanPra@users.noreply.github.com>
2019-10-23 14:01:40 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Work_ffs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Wwwwwwworld!");
         }
     }
 }
